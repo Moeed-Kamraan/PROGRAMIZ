1f2bad478a443a3a7f7b3ad9bc07317e8834cd73
Moeed Kamraan <148672095+Moeed-Kamraan@users.noreply.github.com>
2024-06-21 09:39:53 +0530

    Add files via upload

---
+++ Using_while_loop_print_the_numbers_in_a_range_given_by_user.cpp
@@ -0,0 +1,17 @@
+//while_loop
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num,i;
+    cout<<"Enter the num : ";
+    cin>>num;
+    i=0;
+   while(i<num)
+   { 
+       cout<<i<<"\n";
+       i++;
+   
+   }
+     return 0;
+}
