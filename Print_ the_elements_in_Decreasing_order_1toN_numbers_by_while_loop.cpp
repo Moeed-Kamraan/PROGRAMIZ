f5f7a091661645fda2a53e9b7373fc590515c589
Moeed Kamraan <148672095+Moeed-Kamraan@users.noreply.github.com>
2024-06-21 13:01:12 +0530

    Add files via upload

---
+++ Print_ the_elements_in_Decreasing_order_1toN_numbers_by_while_loop.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int i,n;
+    cout<<"Enter the number : ";
+    cin>>n;
+    i=n;
+    while(i>=1)
+    {
+        cout<<i<<"\t";
+        i--;
+    
+
+    }
+return 0;
+}
