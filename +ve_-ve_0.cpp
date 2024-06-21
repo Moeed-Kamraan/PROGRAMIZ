f87a8033d282175014621bd5f05cf99b711bd9b2
Moeed Kamraan <148672095+Moeed-Kamraan@users.noreply.github.com>
2024-06-21 10:22:28 +0530

    Add files via upload

---
+++ +ve_-ve_0.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+int a;
+cout<<"Enter the number : ";
+cin>>a;
+if(a==0)
+
+    cout<<"The number "<<a <<" is zero ";
+    else if (a<0)
+    
+    cout<< "The number "<<a << " is negative ";
+    else 
+    cout<<"The number "<<a<< " is positive ";
+    
+
+
+    return 0;
+}
