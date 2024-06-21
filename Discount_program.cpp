15cd08e51a027ac0d36756e0457efa47eaf1241c
Moeed Kamraan <148672095+Moeed-Kamraan@users.noreply.github.com>
2024-06-21 10:20:39 +0530

    Add files via upload

---
+++ Discount_program.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main() {
+
+int amount,discount,dis;
+
+cout<<"Enter the total purchased amount : ";
+cin>>amount;
+cout<<"Enter the discount :";
+cin>>dis;
+
+discount=amount*dis/100;
+cout<<"Total purchase : "<<amount<<endl;
+cout<<"Total discount:"<<discount<<endl;
+cout<<"Payable amount : "<<amount-discount;
+return 0;
+}
