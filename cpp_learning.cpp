15bf9637625dc1f856933446f6b4692301729209
Moeed Kamraan <148672095+Moeed-Kamraan@users.noreply.github.com>
2024-06-21 09:21:31 +0530

    Create cpp_learning.cpp

---
+++ cpp_learning.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+cout<<"Hi how are you"<<endl;
+int x,y;
+x=7; //here x=7 means place value 7 in x i,e = is assignment here and not an equal to operator
+y=3;
+// int x=6,y=3;
+cout<<x<<endl;
+cout<<y<<endl;
+
+// cout<<endl;
+x=3,y=4;
+
+cout<<x<<endl;
+cout<<y<<endl;
+cout<<x/y;
+
+x=x+6;//x+=6;
+cout<<endl;
+x=x+1;//x+=1 or x++
+x=8;
+y=3;
+
+cout<<x*y;
+cout<<endl;
+cout<<x%y;
+cout<<endl;
+x=3;
+y=999;
+cout<<x%y;// i,e if x is less than y no matter how munch the answer will be x
+
+
+return 0;
+}
