493f9603b60178f48e5a4f3b1912491e9e75cd3b
Moeed Kamraan <148672095+Moeed-Kamraan@users.noreply.github.com>
2024-06-21 10:19:32 +0530

    Add files via upload

---
+++ do_while_loop.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int i,n;
+    cout<<"Enter the number : ";
+    cin>>n;
+i=0;
+
+do
+{
+    cout<<i<<"\n";
+    i++;
+
+}while(i<=n);
+return 0;
+
+}
