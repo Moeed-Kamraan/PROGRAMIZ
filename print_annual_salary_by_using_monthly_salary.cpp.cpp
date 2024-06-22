28276ff329ee6597be08997004a08f5bd38895ce
Moeed Kamraan <148672095+Moeed-Kamraan@users.noreply.github.com>
2024-06-22 22:08:29 +0530

    Add files via upload

---
+++ print_annual_salary_by_using_monthly_salary.cpp.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+
+int main(){
+
+float m_salary,a_salary;
+
+cout<<"what is your monthly salary : ";
+cin>>m_salary;
+
+a_salary = m_salary*12;
+
+cout<<"Your annual salary is :  "<< a_salary<<endl;
+
+    return 0;
+}
